301fab34262d43ee82082283f2307519e1b94402
Ashwini Jindal <ashwinijindal10@gmail.com>
2018-05-25 12:25:07 +0530

    Update mus_notes.py

--- mus_notes.py
+++ mus_notes.py
@@ -5,22 +5,28 @@ import pysynth_b as psb
 from pygame import mixer
 import winsound
 
+
 w_notes =["a","a#","b","b","c#","d","d#","e","f","f#","g","g#","a","a#","b" ,"c" ,"c#","d","d#" ,"e","f","f#","g","g#" ]
 i_notes= ["S","r","R","g","G","M","m","P","d","D","n","N","S'"]
 
 
+def get_note(c,scl):
+    ind=i_notes.index(c)+w_notes.index(scl)
+    oct=4 if(ind+1) <=12 else 5
+    return w_notes[ind]+str(oct)
 
-duration =4
+
+ocatv = 12
+duration =2
 scale ='c#'
-sargam ='S'  #R G M P D N S
-w_sargam=((w_notes[i_notes.index(c)+w_notes.index(scale)],duration) for c in sargam.split())
+sargam ='S R G M P D N'
+w_sargam=tuple((get_note(c,scale),duration) for c in sargam.split())
 
-print(tuple( w_sargam))
+print( w_sargam)
 
-w_sargam=(('c4', 161),)
 
-psb.make_wav(w_sargam, fn="d://test.wav", leg_stac = .7, bpm = 280)  #tuple( w_sargam)
+
+psb.make_wav(w_sargam, fn="d://test.wav", leg_stac = 1, bpm = 200)  #tuple( w_sargam)
 winsound.PlaySound('d:/test.wav', winsound.SND_ASYNC)
 
-#time.sleep(12)
-lines = sys.stdin.readlines()
+
