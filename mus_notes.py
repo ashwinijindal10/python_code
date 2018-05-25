e5b9e3b516c8ec0e3e13f552f24bf455be897c64
Ashwini Jindal <ashwinijindal10@gmail.com>
2018-05-25 15:01:49 +0530

    Update mus_notes.py
    final working

--- mus_notes.py
+++ mus_notes.py
@@ -1,32 +1,39 @@
 
 
 import time, sys
-import pysynth_b as psb
+import pysynth_p as psb
 from pygame import mixer
 import winsound
 
+w_notes =["c","c#","d","d#","e","f","f#","g","g#","a","a#","b" ]
+i_notes= ["S","r","R","g","G","M","m","P","d","D","n","N"]
 
-w_notes =["a","a#","b","b","c#","d","d#","e","f","f#","g","g#","a","a#","b" ,"c" ,"c#","d","d#" ,"e","f","f#","g","g#" ]
-i_notes= ["S","r","R","g","G","M","m","P","d","D","n","N","S'"]
-
+def default_octav(c) :
+    if("'" in c): #uppper octav 
+        return 4
+    elif ("," in c ):
+        return 2 # lower ocatav 
+    else :
+        return 3 # mid ocatav
 
 def get_note(c,scl):
-    ind=i_notes.index(c)+w_notes.index(scl)
-    oct=4 if(ind+1) <=12 else 5
-    return w_notes[ind]+str(oct)
+    ind=i_notes.index(c.replace("'","").replace(",",""))+w_notes.index(scl)
+    oct= default_octav(c) +(int((ind+1)/12)+(((ind+1)%12) >0 if 1 else 0))
+    rindex=ind if(ind<12) else ind%12 
+    rs= w_notes[rindex]+str(oct)   
+   # print (ind, c, i_notes.index(c.replace("'","").replace(",","")),oct,rs)  
+    return rs
 
 
 ocatv = 12
 duration =2
 scale ='c#'
-sargam ='S R G M P D N'
+sargam ="S r G M P D n S' S' n D P M G r S"
 w_sargam=tuple((get_note(c,scale),duration) for c in sargam.split())
 
 print( w_sargam)
 
-
-
-psb.make_wav(w_sargam, fn="d://test.wav", leg_stac = 1, bpm = 200)  #tuple( w_sargam)
+psb.make_wav(w_sargam, fn="d://test.wav",  bpm = 200)  #tuple( w_sargam)
 winsound.PlaySound('d:/test.wav', winsound.SND_ASYNC)
 
-
+#C#    D#    F      F#     G#    a#    C      C#
